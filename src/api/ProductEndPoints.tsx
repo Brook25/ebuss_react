762a77320e6ba876ef1b7a83166735c2d1a43b29
Brook25 <bekelebrook24@gmail.com>
2026-05-19 23:09:33 +0300

    Doing payment checkout page

--- src/api/ProductEndPoints.tsx
+++ src/api/ProductEndPoints.tsx
@@ -1,24 +1,28 @@
-type epFunction = ((arg?: string) => string);
+interface urlParams {
+    identifier?: string;
+    queryParams: string | null;
+} 
+type epFunction = ((urlParams: urlParams) => string);
 
 
 const ProductEndPoints: Record<string, epFunction> = {
-    'all': (queryParams?: string) => `/products/all${queryParams ? `?${queryParams}` : ''}`,
-    'category': (categoryId?: string, queryParams?: string) => `/products/category/${categoryId}${queryParams ? `?${queryParams}` : ''}`,
-    'search': (queryParams?: string) => `/products/search?q=${queryParams}`,
-    'subCategory': (subcategoryId?: string, queryParams?: string) => `/products/subcategory/${subcategoryId}${queryParams ? `?${queryParams}` : ''}`,
-    'store': (queryParams?: string) => `/products/store/${queryParams ? `?${queryParams}` : ''}`,
-    'popular': (pathSegment?: string, queryParams?: string) => `/products/popular/${pathSegment}/${queryParams ? `?${queryParams}` : ''}` 
+    'all': ({ queryParams }) => `/products/all${queryParams ? `?${queryParams}` : ''}`,
+    'category': ({ identifier, queryParams }) => `/products/category/${identifier}${queryParams ? `?${queryParams}` : ''}`,
+    'search': ({ queryParams }) => `/products/search?q=${queryParams}`,
+    'subCategory': ({ identifier, queryParams }) => `/products/subcategory/${identifier}${queryParams ? `?${queryParams}` : ''}`,
+    'store': ({ queryParams }) => `/products/store/${queryParams ? `?${queryParams}` : ''}`,
+    'popular': ({ identifier, queryParams }) => `/products/popular/${identifier}/${queryParams ? `?${queryParams}` : ''}` 
 };
 
-const resolveEndpoint = (resource: string, identifier: string | null = null) => {
+const resolveEndpoint = (resource: string, identifier: string | null = null, queryParams: string | null) => {
     const epResolver = ProductEndPoints[resource as keyof typeof ProductEndPoints];
+    const urlParams = { queryParams, ...( identifier !== null ? { identifier } : {}) };
     if (! (typeof epResolver === 'function')) {
       console.error(`Endpoint for resource '${resource}' is not a function.`);
       return null;
     }
     try {
-    if (identifier !== null) return epResolver(identifier);
-    return epResolver();
+    if (identifier !== null) return epResolver({ identifier, queryParams });
     }
     catch (error) {
         console.error(`Error resolving endpoint for resource ${resource} with error: ${error}`);
